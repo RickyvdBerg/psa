5dffbc063d9b34d99cc041ebcf00220c754f3034
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2023-08-24 16:08:03 +0200

    Update psa.ps1

--- psa.ps1
+++ psa.ps1
@@ -1,4 +1,7 @@
 $Desktop = [Environment]::GetFolderPath([Environment+SpecialFolder]::Desktop)
 $Documents = [Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
 $Downloads = [Environment]::ExpandEnvironmentVariables("%USERPROFILE%\Downloads")
-Invoke-WebRequest -Uri "https://i.imgur.com/NpJHp6L.png" -OutFile "$Desktop\WarningPCUnlocked.png"; Invoke-WebRequest -Uri "https://i.imgur.com/MhU0rkN.jpg" -OutFile "$Desktop\egg1.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/yifHSyV.jpg" -OutFile "$Documents\egg2.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/ZUTHKPK.jpg" -OutFile "$Downloads\egg3.jpg"; & "$Desktop\WarningPCUnlocked.png"
+Invoke-WebRequest -Uri "https://i.imgur.com/NpJHp6L.png" -OutFile "$Desktop\WarningPCUnlocked.png"; Invoke-WebRequest -Uri "https://i.imgur.com/MhU0rkN.jpg" -OutFile "$Desktop\egg1.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/yifHSyV.jpg" -OutFile "$Documents\egg2.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/ZUTHKPK.jpg" -OutFile "$Downloads\egg3.jpg"; & "$Desktop\WarningPCUnlocked.png";
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("{F11}")
+
