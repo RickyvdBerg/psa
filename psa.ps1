edbab2136948f6d037548d90b3414f8b3ab8b753
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2023-08-24 13:55:11 +0200

    Create psa.ps1

---
+++ psa.ps1
@@ -0,0 +1,4 @@
+$Desktop = [Environment]::GetFolderPath([Environment+SpecialFolder]::Desktop)
+$Documents = [Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
+$Downloads = [Environment]::ExpandEnvironmentVariables("%USERPROFILE%\Downloads")
+Invoke-WebRequest -Uri "https://i.imgur.com/NpJHp6L.png" -OutFile "$Desktop\WarningPCUnlocked.png"; Invoke-WebRequest -Uri "https://i.imgur.com/MhU0rkN.jpg" -OutFile "$Desktop\egg1.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/yifHSyV.jpg" -OutFile "$Documents\egg2.jpg"; Invoke-WebRequest -Uri "https://i.imgur.com/ZUTHKPK.jpg" -OutFile "$Downloads\egg3.jpg"; & "$Desktop\WarningPCUnlocked.png"
